c329056025fc81dc5cca02d031c82a48e380787f
Arif <arifonly4@gmail.com>
2024-03-22 10:59:24 +0530

    learn

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   myValue: string = "X-yug";
-
   name:string = "Goldsikka"
-
   enabled: boolean = false;
   yourValue:string = "Information"
-
 
   manyclasses ={
     name1 : true,
@@ -26,8 +23,24 @@ export class HomeComponent {
     'background-color': 'blue', 'font-size': '14px',
     'border': '5px solid red '
   }
+  show = true;
+  color = 'green';
+  color2 = 'purple';
 
-  show =false;
+  users = ['perry','arif', 'rohit', 'arman']
+  userDeatails=[
+    {name:'perry', age:23, address:'Aus'},
+    {name:'arif',  age:21, address:'Hyd'},
+    {name:'rohit', age:26, address:'Pune'},
+    {name:'arman', age:15, address:'Begu'},
 
 
+  ]
+
+  useremp =[
+    {name:'sohan', phone:'889900',socialacc:['fb','insta','linkedIn']},
+    {name:'alish', phone:'889910', socialacc:['Insta','youtube','linkedIn']},
+    {name:'rajesh', phone:'909400', socialacc:['fb','email','insta']},
+    {name:'aoora', phone:'889930', socialacc:['snapchat','insta','linkedIn']},
+  ]
 }
